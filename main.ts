30acdf01fc38b7a8ce766715c7315b23184c4bfb
Peter Heldens <peter.heldens@live.nl>
2020-06-14 19:25:22 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("distance", distance)
+    distance += 1
+    basic.showIcon(IconNames.Yes)
+    basic.showNumber(distance)
+})
+input.onButtonPressed(Button.AB, function () {
+    distance = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(distance)
+})
+radio.onReceivedValue(function (name, value) {
+    serial.writeNumbers([value, radio.receivedPacket(RadioPacketProperty.SignalStrength)])
+    serial.writeLine("")
+    basic.showNumber(value)
+})
+let distance = 0
+distance = 0
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.showNumber(distance)
